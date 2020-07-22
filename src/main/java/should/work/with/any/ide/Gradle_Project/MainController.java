2557a8b8820745cc0e0da9431d88e16118826a55
aneesh anil <aneeshanil008@gmail.com>
2020-07-22 23:20:23 +0530

    first commit

---
+++ src/main/java/should/work/with/any/ide/Gradle_Project/MainController.java
@@ -0,0 +1,13 @@
+package should.work.with.any.ide.Gradle_Project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
